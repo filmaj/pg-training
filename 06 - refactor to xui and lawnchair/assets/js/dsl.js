2c05c1ae27bf79704498913bc8fdc30d8d6c421d
filmaj <filip.maj@nitobi.com>
2010-08-18 10:51:12 -0700

    Small tweak: click to touchstart.

--- 06 - refactor to xui and lawnchair/assets/js/dsl.js
+++ 06 - refactor to xui and lawnchair/assets/js/dsl.js
@@ -28,7 +28,7 @@ var run = function(application) {
 
 // reg a click to [id]_button, displays id (if it exists) and executes callback (if it exists)
 , when = function(id, callback) {
-    x$(id + '_button').click(function () {
+    x$(id + '_button').on('touchstart', function () {
         if (x$(id).length > 0)
             display(id);
         if (callback)
