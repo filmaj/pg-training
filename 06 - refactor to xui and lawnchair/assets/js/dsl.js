98fd2e5229fb861f4d35b3fdb27e054c6b782871
filmaj <filip.maj@nitobi.com>
2010-08-18 11:02:16 -0700

    Simplified load logic.

--- 06 - refactor to xui and lawnchair/assets/js/dsl.js
+++ 06 - refactor to xui and lawnchair/assets/js/dsl.js
@@ -1,18 +1,12 @@
 // the app method accepts a fn to invoke on init unobtrusively 
 var run = function(application) {
-    x$(window).on('load', function() {
-        if (document.readyState == 'loaded' || document.readyState == 'complete') {
-            application.call(this);
-        } else {
-        	if (navigator.userAgent.indexOf('Browzr') > -1) {
-        	    // blackberry
-        		setTimeout(application, 250)	
-        	} else {
-        	    // final callback?
-        		x$(document).on('deviceready', application, false);
-        	}
-        };
-    });
+    if (navigator.userAgent.indexOf('Browzr') > -1) {
+        // blackberry
+        setTimeout(application, 250)	
+    } else {
+        // attach to deviceready event, which is fired when phonegap is all good to go.
+        x$(document).on('deviceready', application, false);
+    }
 }
 
 // throw our settings into a lawnchair
